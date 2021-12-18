a4561ff1d5e0a561047f26c16048450bc84c1457
yonin7 <yonin789@walla.com>
2021-12-18 22:30:47 +0200

    img alt fixed

--- src/components/Dice/index.js
+++ src/components/Dice/index.js
@@ -5,6 +5,7 @@ function Dice(props) {
   return (
     <img
       className="dice"
+      alt=""
       src={`dice-${props.cubeResult}.png`}
       style={{ display: props.cubeResult === 0 ? 'none' : 'block' }}
     />
